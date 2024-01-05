a48cd3f3257587e84d80372f981f4184a947daa9
Abhijeet Waghchaure <abhijeetswaghchaure@gmail.com>
2024-01-05 18:29:30 +0530

    workflow of the program (project)

---
+++ project.js
@@ -0,0 +1,18 @@
+//the below are the steps in which the actual workflow of the project will work, please note that
+
+//STEP 1 - deposit some money
+//STEP 2 - determine the number of lines to bet on
+//STEP 3 - collect a bet amount
+//STEP 4 - spin the slot machine
+//STEP 5 - check if the user won
+//STEP 6 - give the user their winnings
+//STEP 7 - play again
+
+function deposit(){
+    return 1;
+
+}
+
+const deposit = () => {
+    
+}
