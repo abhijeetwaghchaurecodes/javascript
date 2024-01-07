67d6a8b82cc9011442c2e0aa4626ecfcc76ded69
Abhijeet Waghchaure <abhijeetswaghchaure@gmail.com>
2024-01-07 18:38:14 +0530

    third commit
    mostly the work is done. SPIN, BET, TRANSPOSE (for the rows to be displayed to the use). those functions are implemented

--- project.js
+++ project.js
@@ -16,6 +16,23 @@
 const prompt = require("prompt-sync")();
 
 
+const ROWS = 3;
+const COLS = 3;
+
+const SYMBOLS_COUNT = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
+const SYMBOL_VALUES = {
+    "A": 5,
+    "B": 4,
+    "C": 3,
+    "D": 2
+}
+
 //the below function will handle the deposit process with possible error handling
 const deposit = () => {
 
@@ -48,5 +65,63 @@ const getNumberOfLines = () => {
     }
 }
 
-const depositAmount = deposit();
+
+//the below function contains the bet system. the bet amount should be valid numeric value, positive and should be greater than balance 
+const getBet = (balance, lines) => {
+    while(true){
+        const bet = prompt("Enter the bet amount per line: ");
+        const numberBet = parseFloat(bet);
+
+        if(isNaN(numberBet) || numberBet <= 0 || numberBet > (balance / lines)){
+            console.log("Invalid bet, try again.");
+        }else{
+            return numberBet;
+        }
+    }
+}
+
+const spin = () => {
+    const symbols = [];  //this array contains all the available symbols to use
+    for ([symbol, count] of Object.entries(SYMBOLS_COUNT)){
+        //console.log(symbol, count);
+        for (let i = 0; i < count; i++){
+            symbols.push(symbol);
+        }
+    }
+
+    const reels = [[], [], []];
+    for(let i = 0; i < COLS; i++){
+        const reelSymbols = [...symbols];  //here in this array we're using the symbols in the 'symbols' array so that we can use it here
+            for(let j = 0; j < ROWS; j++){
+                const randomIndex = Math.floor(Math.random() * reelSymbols.length);
+                const selectedSymbol = reelSymbols[randomIndex];
+                reels[i].push(selectedSymbol);
+                reelSymbols.splice(randomIndex, 1);
+            }
+    }
+
+    return reels;
+};
+
+const transpose = (reels) => {
+    const rows = [];
+
+    for(let i = 0; i < ROWS; i++){
+        rows.push([]);
+        for(let j = 0; j < COLS; j++){
+            rows[i].push(reels[j][i]);
+        }
+    }
+
+    return rows;
+}
+
+
+console.log(reels);
+
+let balance = deposit();
 const numberOfLines = getNumberOfLines();
+const bet = getBet(balance, numberOfLines);
+const reels = spin();
+
+const rows = transpose(reels);
