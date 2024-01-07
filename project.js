5cfc9629b62cf0692dfb87dd6dbe0a0cce508e83
Abhijeet Waghchaure <abhijeetswaghchaure@gmail.com>
2024-01-07 14:45:22 +0530

    second commit
    this contains the 2 basic functions of the casino game i.e deposit amt and no. of lines prediction

--- project.js
+++ project.js
@@ -8,11 +8,45 @@
 //STEP 6 - give the user their winnings
 //STEP 7 - play again
 
-function deposit(){
+/*function deposit(){
     return 1;
 
+}*/
+
+const prompt = require("prompt-sync")();
+
+
+//the below function will handle the deposit process with possible error handling
+const deposit = () => {
+
+    while(true){
+        const depositAmount = prompt("Enter a deposit amount: ");
+        const numberDepositAmount = parseFloat(depositAmount);
+
+        if(isNaN(numberDepositAmount) || numberDepositAmount <= 0){
+            console.log("Invalid deposit amount, try again.");
+        }   else {
+            return numberDepositAmount;
+        }
+    }
+    
+};
+
+
+//the below function will handle the number of lines the user is trying to predict 
+const getNumberOfLines = () => {
+    while(true){
+        const lines = prompt("Enter number of lines (1-3): ");
+        const numberOfLines = parseFloat(lines);
+
+        if(isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3){
+            console.log("Invalid number of lines, try again.");
+        } else {
+            return numberOfLines;
+        }
+
+    }
 }
 
-const deposit = () => {
-    
-}
+const depositAmount = deposit();
+const numberOfLines = getNumberOfLines();
